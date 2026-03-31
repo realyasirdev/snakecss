193e6826bd36b2d276fac8a84859bb3d8ff2c283
realyasirdev <yasiralikhanrazakhail@gmail.com>
2026-03-31 15:21:10 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import List from "./components/List";
+
+export default function App(){
+    return(
+        <>
+        <Header/>
+        <List/>
+        </>
+    );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header(){
+    return(
+        <>
+        <div class="page">
+    <div class="image-card">
+      <img
+        src="https://play-lh.googleusercontent.com/6kWQcyohDGTWWZpO4raWWQh3PrMvD7Eq6DFN7vgk6C37Hk38m-QQbScDwyUpgJ9BBdv5"
+        alt="Reference Image"
+      />
+    </div>
+  </div>
+        </>
+    );
+}
